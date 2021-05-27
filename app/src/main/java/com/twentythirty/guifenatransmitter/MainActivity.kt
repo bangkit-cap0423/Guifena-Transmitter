802dfc770af57542db07a7672e1cc5b482feb52e
Taufan <taufanderazor@gmail.com>
2021-05-27 15:52:16 +0700

    berhasil add service

--- app/src/main/java/com/twentythirty/guifenatransmitter/MainActivity.kt
+++ app/src/main/java/com/twentythirty/guifenatransmitter/MainActivity.kt
@@ -1,15 +1,77 @@
 package com.twentythirty.guifenatransmitter
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.twentythirty.guifenatransmitter.databinding.ActivityMainBinding
+import com.twentythirty.guifenatransmitter.services.RecordService
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val CHANNEL_ID = "Ntap"
+        const val CHANNEL_NAME = "Info"
+
+    }
+
+    private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
     private lateinit var binding: ActivityMainBinding
+    private val REQUEST_RECORD_AUDIO_PERMISSION = 200
+    private var permissionToRecordAccepted = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //lala
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION)
+        binding.btnStart.setOnClickListener {
+            startService()
+        }
+        binding.btnStop.setOnClickListener {
+            stopService()
+        }
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel: NotificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_LOW
+            )
+
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager?.createNotificationChannel(serviceChannel)
+        }
+    }
+
+    fun startService() {
+        val myServiceIntent = Intent(this, RecordService::class.java)
+        ContextCompat.startForegroundService(this, myServiceIntent)
+    }
+
+    fun stopService() {
+        val serviceIntent = Intent(this, RecordService::class.java)
+        stopService(serviceIntent)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        } else {
+            false
+        }
+        if (!permissionToRecordAccepted) finish()
     }
 }
---
+++ app/src/main/java/com/twentythirty/guifenatransmitter/services/RecordService.kt
@@ -0,0 +1,144 @@
+package com.twentythirty.guifenatransmitter.services
+
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.media.MediaRecorder
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.twentythirty.guifenatransmitter.MainActivity
+import com.twentythirty.guifenatransmitter.R
+import java.io.File
+import java.io.IOException
+import kotlin.math.abs
+import kotlin.math.log10
+
+
+class RecordService : Service() {
+    private lateinit var mRecorder: MediaRecorder
+    private var amplitudeValue = 0
+    private var amplitudeDb = 0.0
+    private val UPDATE_INTERVAL = 1000L
+    private lateinit var mainHandler: Handler
+    private lateinit var file: File
+    private lateinit var mp: MediaPlayer
+    private var isRecording = false
+    private val updateAmplitude = object : Runnable {
+        override fun run() {
+            val tempAplitude = mRecorder.maxAmplitude
+            if (tempAplitude > amplitudeValue) {
+                amplitudeValue = tempAplitude
+            }
+            amplitudeDb = 20 * log10(abs(amplitudeValue).toDouble())
+            updateNotification("Recording...", "Amplitude: $amplitudeDb")
+            mainHandler.postDelayed(this, UPDATE_INTERVAL)
+        }
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val input = intent?.getStringExtra("wawawa")
+        mainHandler = Handler(Looper.getMainLooper())
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
+        val notification = NotificationCompat.Builder(this, MainActivity.CHANNEL_ID)
+            .setContentTitle("Ini Forefround")
+            .setContentText(input)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        startForeground(1, notification)
+        startRecord()
+        return START_NOT_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+    override fun onDestroy() {
+        if (isRecording) {
+            mRecorder.stop()
+        }
+        if (mp.isPlaying) {
+            mp.stop()
+        }
+        mainHandler.removeCallbacksAndMessages(null)
+        super.onDestroy()
+    }
+
+    private fun startRecord() {
+        if (::mp.isInitialized) {
+            if (mp.isPlaying) {
+                mp.stop()
+            }
+        }
+        isRecording = true
+        file = File(this.filesDir, "tes.m4a")
+        mRecorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+            setAudioEncodingBitRate(128000)
+            setAudioSamplingRate(44100)
+            setOutputFile(file.toString())
+        }
+        try {
+            mRecorder.prepare()
+            mRecorder.start()
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        mainHandler.post(updateAmplitude)
+        mainHandler.postDelayed({ stopRecord() }, 10000L)
+    }
+
+    private fun stopRecord() {
+        if (isRecording) {
+            mRecorder.stop()
+            isRecording = false
+        }
+        if (amplitudeDb > 50.0) {
+            //Upload to cloud
+            updateNotification("Playing sound..", "denger")
+            mp = MediaPlayer()
+            mp.apply {
+                setDataSource(file.toString())
+                prepare()
+                start()
+            }
+            //"ADD COROUTINE FOR UPLOADING")
+            mainHandler.removeCallbacks(updateAmplitude)
+            mRecorder.release()
+            mainHandler.postDelayed({ startRecord() }, 10000L)
+        } else {
+            mainHandler.removeCallbacks(updateAmplitude)
+            mRecorder.release()
+            mainHandler.postDelayed({ startRecord() }, 10000L)
+        }
+
+    }
+
+    fun updateNotification(title: String, subTitle: String) {
+        Log.d("farin", "cantik")
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
+        val notification = NotificationCompat.Builder(this, MainActivity.CHANNEL_ID)
+            .setContentTitle(title)
+            .setContentText(subTitle)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentIntent(pendingIntent)
+            .setSound(null)
+            .build()
+        with(NotificationManagerCompat.from(this)) {
+            notify(1, notification)
+        }
+    }
+}
